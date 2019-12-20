69f06c2565750b480d2bfcd273910db3a9f8d43a
sammywork <sammysteiner@gmail.com>
2019-12-20 14:01:00 -0500

    adding login fetch request to api/index

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,27 @@
-const SITEMINDER = ''
+const SITEMINDER = 'https://msdlvw-hhsces01.csc.nycnet/siteminderagent/forms/loginpages/wc/login.fcc'
 const EMERGENCY_APP_WEB = ''
 
 export function login(params){
-
+  var formDetails = {
+    'user': params.user,
+    'password': params.password,
+    'agency': params.agency,
+    'rememberMe':'false',
+    'loginExe':'Sign In',
+    'target':'/HHSWorkerConnectWeb/homePageRouter.jsp?agency=doitt',
+    'users':'users',
+    'smauthreason':'0'
+  };
+  const formBody = Object.keys(formDetails).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(formDetails[key])).join('&');
+  return fetch(SITEMINDER, {
+      headers: {
+      'Content-Type':'application/x-www-form-urlencoded',
+      'Referer': 'https://msdlvw-hhsces01.csc.nycnet/'
+    },
+    method: 'POST',
+    body: formBody
+  })
+  .then( res => {console.log(res)} )
 }
 
 export function search(){
